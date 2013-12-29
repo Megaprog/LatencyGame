b274ae40916a4e28e301b3f0f510fefbbff9cb8b
Tomas Shestakov <shestakovte@gmail.com>
2013-12-29 17:22:22 +0400

    added GameOver message

--- src/main/scala/game/ManagerActor.scala
+++ src/main/scala/game/ManagerActor.scala
@@ -5,6 +5,8 @@
 package game
 
 import akka.actor.{ActorRef, Props, ActorRefFactory, Actor}
+import messages.{GameOver, GameRequest}
+import org.slf4j.LoggerFactory
 
 /**
  * User: Tomas
@@ -12,13 +14,17 @@ import akka.actor.{ActorRef, Props, ActorRefFactory, Actor}
  * Time: 12:21
  */
 class ManagerActor(players: Int, gameActorFactory: (Seq[ActorRef]) => ActorRef) extends Actor {
+  import ManagerActor.logger
 
   def receive: Actor.Receive = {
-    case _ =>
+    case request @ GameRequest =>
+      logger.info(s"$request from $sender")
+    case result @ GameOver => logger.info(result.toString())
   }
 }
 
 object ManagerActor {
+  val logger = LoggerFactory.getLogger(classOf[ManagerActor])
 
   def create(actorRefFactory: ActorRefFactory, players: Int, gameActorFactory: (Seq[ActorRef]) => ActorRef) =
     actorRefFactory.actorOf(Props(classOf[ManagerActor], players, gameActorFactory))
---
+++ src/main/scala/messages/GameOver.scala
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2013 Tomas Shestakov. <https://github.com/Megaprog/LatencyGame>
+ */
+
+package messages
+
+import akka.actor.ActorRef
+
+/**
+ * User: Tomas
+ * Date: 29.12.13
+ * Time: 17:09
+ */
+case class GameOver(players: Seq[ActorRef], winner: ActorRef)
