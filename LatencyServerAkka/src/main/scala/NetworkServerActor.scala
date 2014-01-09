998fe5257c85ba56b1cb68bd460c0ba43988d002
Tomas Shestakov <shestakovte@gmail.com>
2014-01-09 16:46:23 +0400

    removed debug code

--- LatencyServerAkka/src/main/scala/NetworkServerActor.scala
+++ LatencyServerAkka/src/main/scala/NetworkServerActor.scala
@@ -35,21 +35,19 @@ class NetworkServerActor(port: Int, clientFactory: (ActorRefFactory, Init[Within
 
   def bound(): Actor.Receive = {
     case connected @ Connected(remote, local) =>
-//      val init = TcpPipelineHandler.withLogger(log,
-//        new StringByteStringAdapter("utf-8") >>
-//        new TelnetNegotiationCutter >>
-//        new TcpReadWriteAdapter >>
-//        new BackpressureBuffer(lowBytes = 100, highBytes = 1000, maxBytes = 1000000)
-//      )
-//
-//      val connection = sender
-//      val client = clientFactory(context, init)
-//      val pipeline = context.actorOf(Props(classOf[ClosablePipelineHandler[_, _, _]], init, connection, client))
-//
-//      connection ! Register(pipeline)
-//      pipeline ! connected //to inform client about connection
+      val init = TcpPipelineHandler.withLogger(log,
+        new StringByteStringAdapter("utf-8") >>
+        new TelnetNegotiationCutter >>
+        new TcpReadWriteAdapter >>
+        new BackpressureBuffer(lowBytes = 100, highBytes = 1000, maxBytes = 1000000)
+      )
 
-      //sender ! Register(context.actorOf(Props[DummyHandler]))
+      val connection = sender
+      val client = clientFactory(context, init)
+      val pipeline = context.actorOf(Props(classOf[ClosablePipelineHandler[_, _, _]], init, connection, client))
+
+      connection ! Register(pipeline)
+      pipeline ! connected //to inform client about connection
   }
 }
 
@@ -57,11 +55,4 @@ object NetworkServerActor {
 
   def create(actorSystem: ActorRefFactory, port: Int, clientFactory: (ActorRefFactory, Init[WithinActorContext, String, String]) => ActorRef) =
     actorSystem.actorOf(Props(classOf[NetworkServerActor], port, clientFactory).withDispatcher("akka.io.pinned-dispatcher"))
-}
-
-class DummyHandler extends Actor {
-
-  def receive: Actor.Receive = {
-    case msg @ _ => println(msg)
-  }
 }
